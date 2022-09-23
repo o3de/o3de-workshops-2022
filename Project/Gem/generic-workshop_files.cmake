81fe3d2569c64ff153c2211b35dbf2be90a342fe
Jonny Galloway <gallowj@amazon.com>
2022-09-23 17:32:48 -0500

    adding project stubs
    Signed-off-by: Jonny Galloway <gallowj@amazon.com>

---
+++ Project/Gem/generic-workshop_files.cmake
@@ -0,0 +1,7 @@
+
+set(FILES
+    Include/generic-workshop/generic-workshopBus.h
+    Source/generic-workshopSystemComponent.cpp
+    Source/generic-workshopSystemComponent.h
+    enabled_gems.cmake
+)
